e606a8e28ebe5642913240f6aacabf11225b52b5
constancecchen <constance.chen.3@gmail.com>
2017-01-26 17:44:41 -0800

    Remove unnecessary quotation marks in AMD module registration

--- src/objectFitPolyfill.basic.js
+++ src/objectFitPolyfill.basic.js
@@ -13,7 +13,7 @@
 (function (root, factory) {
   if (typeof define === "function" && define.amd) {
     // AMD. Register as an anonymous module.
-    define([""], function () {
+    define([], function () {
       return (root.objectFitPolyfill = factory());
     });
   } else if (typeof module === "object" && module.exports) {
